60d758942c1e084df3bd0271d885f13928a779b2
Jonathan García <jonathan.garcia.gil.15@gmail.com>
2022-03-27 20:12:00 -0600

    first commit

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 "# vuex-LaunchX" 
+"# vuex-LaunchX" 
---
+++ src/views/HomeView.vue
@@ -0,0 +1,49 @@
+<template>
+  <div class="home">
+    <img alt="Vue logo" src="../assets/logo.png">
+
+    <Counter/>
+    
+    <Botones/>    
+
+    <Cuadrado/>
+
+  </div>
+</template>
+
+<script>
+
+import Counter from "../components/contador.vue";
+import Cuadrado from "../components/cuadrado.vue";
+import Botones from "../components/botones.vue";
+
+export default {
+  name: 'HomeView',
+  components:{
+    Counter,
+    Cuadrado,
+    Botones,
+  }
+}
+</script>
+
+<style>
+
+  div{
+    margin-bottom: 10px;
+  }
+
+  .counter{
+    font-size: 80px;
+  }
+
+  .botones button{
+    font-size: 40px;
+    width: 100px;
+    margin: 0 10px;
+  }
+
+  .cuadrado{
+    font-size: 25px;
+  }
+</style>
